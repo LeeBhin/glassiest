ad9e930879767f3fb54dbe32676c13cb7dc4b15b
LeeBhin <bini4039@naver.com>
2023-12-06 20:58:50 +0900

    image rotate & resize

--- script.js
+++ script.js
@@ -27,17 +27,21 @@ video.addEventListener("play", () => {
       .withFaceExpressions();
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
     canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-    // faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawDetections(canvas, resizedDetections);
     // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
     // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-    resizedDetections.forEach((detection) => {
-      const leftEye = detection.landmarks.getLeftEye();
-      const rightEye = detection.landmarks.getRightEye();
 
-      // 눈의 위치 출력
-      drawEyePosition(rightEye);
+    resizedDetections.forEach((detection) => {
+      // landmarks 객체가 있는지 확인
+      const rightEye = detection.landmarks.getRightEye();
+      const leftEye = detection.landmarks.getLeftEye();
+      const faceWidth = detection.detection.box.width / 1.2;
+      const faceHeight = detection.detection.box.height / 3;
+
+      // 눈의 위치와 얼굴 크기에 맞게 이미지 조절
+      drawEyePosition(leftEye, rightEye, faceWidth, faceHeight);
     });
-  }, 10);
+  }, 0);
 });
 
 // More API functions here:
@@ -127,15 +131,6 @@ async function detectEyePositions() {
   await faceapi.nets.faceLandmark68Net.loadFromUri("/models");
   const video = document.getElementById("webcam");
   const detections = await faceapi.detectAllFaces(video).withFaceLandmarks();
-
-  detections.forEach((detection) => {
-    const landmarks = detection.landmarks;
-    const leftEye = landmarks.getLeftEye();
-    const rightEye = landmarks.getRightEye();
-
-    console.log("Left Eye Position:", leftEye);
-    console.log("Right Eye Position:", rightEye);
-  });
 }
 
 // 이미지를 미리 로드합니다.
@@ -148,15 +143,63 @@ eyeImage.onload = function () {
   eyeImage.height = 90; // 원하는 높이
 };
 
-// 눈의 위치에 이미지를 그리는 함수
-function drawEyePosition(eye) {
+function drawEyePosition(leftEye, rightEye, faceWidth, faceHeight) {
   const canvas = document.querySelector("canvas");
   const context = canvas.getContext("2d");
 
-  // 이미지의 중심이 눈의 위치에 오도록 조정
-  const eyeX = eye[0].x - eyeImage.width + 90;
-  const eyeY = eye[0].y - eyeImage.height / 2 + 20;
+  // 스케일 계수 조정
+  const scale = 1.0;
+  const scaleWidth = (faceWidth / eyeImage.width) * scale;
+  const scaleHeight = (faceHeight / eyeImage.height) * scale;
+  const scaledWidth = eyeImage.width * scaleWidth;
+  const scaledHeight = eyeImage.height * scaleHeight;
 
-  // 이미지 그리기
-  context.drawImage(eyeImage, eyeX, eyeY, eyeImage.width, eyeImage.height);
+  // 왼쪽 눈과 오른쪽 눈의 중심 좌표 계산
+  const leftEyeCenter = getEyeCenter(leftEye);
+  const rightEyeCenter = getEyeCenter(rightEye);
+
+  // 양쪽 눈의 중간 지점 계산
+  const eyesCenterX = (leftEyeCenter.x + rightEyeCenter.x) / 2;
+  const eyesCenterY = (leftEyeCenter.y + rightEyeCenter.y) / 2;
+
+  // 이미지가 양쪽 눈의 중간에 오도록 조정
+  const eyeX = eyesCenterX - scaledWidth / 2;
+  const eyeY = eyesCenterY - scaledHeight / 2;
+
+  // 얼굴 기울기 계산
+  const angle = Math.atan2(
+    rightEye[0].y - leftEye[0].y,
+    rightEye[0].x - leftEye[0].x
+  );
+
+  // 캔버스에 이미지를 그리기 전에 캔버스 상태 저장
+  context.save();
+
+  // 캔버스 중심을 이미지의 중심으로 이동
+  context.translate(eyesCenterX, eyesCenterY);
+
+  // 이미지 회전
+  context.rotate(angle);
+
+  // 이미지를 원래 위치로 되돌리고 그리기
+  context.drawImage(
+    eyeImage,
+    -scaledWidth / 2,
+    -scaledHeight / 2,
+    scaledWidth,
+    scaledHeight
+  );
+
+  // 캔버스 상태를 이전 상태로 복원
+  context.restore();
+}
+
+function getEyeCenter(eye) {
+  let centerX = 0;
+  let centerY = 0;
+  eye.forEach((point) => {
+    centerX += point.x;
+    centerY += point.y;
+  });
+  return { x: centerX / eye.length, y: centerY / eye.length };
 }
